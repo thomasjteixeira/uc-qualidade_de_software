6a0b9edfb6d199dd9c3c852f74bb715b261c4177
Thómas Jéfferson <thomasjteixeira@gmail.com>
2024-05-16 20:31:10 -0300

    melhorias no Boleto.js e Conta.js

--- gestao_boletos/Boleto.js
+++ gestao_boletos/Boleto.js
@@ -1,28 +1,54 @@
 class Boleto {
+  static currentId = 1;
+
   constructor(valor, dataVencimento, conta) {
+    if (!conta) {
+      throw new Error('Boleto deve estar associado a uma conta.');
+    }
+
+    this.id = Boleto.currentId++;
     this.valor = valor;
     this.dataVencimento = dataVencimento;
     this.conta = conta;
     this.pago = false;
-    // this.status = 'ABERTO' || 'PAGO' || 'CANCELADO' || 'VENCIDO';
   }
 
   pagar() {
-    if (!this.pago) {
+    if (this.pago) {
+      throw new Error('Este boleto já foi pago.');
+    } else {
       this.conta.creditar(this.valor);
       this.pago = true;
       console.log(`Boleto no valor de R$${this.valor} pago com sucesso na conta ${this.conta.id}.`);
-    } else {
-      console.log('Este boleto já foi pago.');
     }
   }
 
   cancelar() {
-    if (!this.pago) {
-      console.log('Boleto cancelado com sucesso.');
+    if (this.pago) {
+      throw new Error('Não é possível cancelar um boleto já pago.');
     } else {
-      console.log('Não é possível cancelar um boleto já pago.');
+      console.log('Boleto cancelado com sucesso.');
     }
+  }
+
+  // Método estático que busca um boleto por ID
+  static buscarPorId(contas, boletoId) {
+    return contas.flatMap(c => c.boletos).find(b => b.id == boletoId);
+  }
+
+  // Método toJSON que será chamado quando o objeto for serializado para JSON
+  // A serialização é o processo de transformar um objeto em uma string que pode ser armazenada ou transmitida.
+  // No caso de JavaScript, a serialização geralmente se refere à conversão de um objeto JavaScript em uma string JSON.
+  // Este método controla como o objeto é serializado, retornando um novo objeto que contém apenas as propriedades que queremos incluir na string JSON.
+  toJSON() {
+    // Retorna um novo objeto contendo apenas as propriedades que queremos incluir na string JSON
+    return {
+      id: this.id,
+      valor: this.valor,
+      dataVencimento: this.dataVencimento,
+      pago: this.pago,
+      contaId: this.conta.id
+    };
   }
 }
 
--- gestao_boletos/Conta.js
+++ gestao_boletos/Conta.js
@@ -7,13 +7,14 @@ const Banco = {
 };
 
 class Conta {
-  constructor(id, saldo, numeroConta, nomeBanco) {
+  static currentId = 1;
 
+  constructor(saldo, numeroConta, nomeBanco) {
     if (!Object.keys(Banco).includes(nomeBanco)) {
       throw new Error('Banco inválido');
     }
 
-    this.id = id;
+    this.id = Conta.currentId++;
     this.saldo = saldo;
     this.numeroConta = numeroConta;
     this.nomeBanco = Banco[nomeBanco];
@@ -36,6 +37,26 @@ class Conta {
     console.log(`Quantidade de boletos: ${this.boletos.length}`);
     console.log(`Saldo: R$${this.saldo}`);
     console.log(`-----------------------------------------`);
+  }
+
+  // Método toJSON que será chamado quando o objeto for serializado para JSON
+  // A serialização é o processo de transformar um objeto em uma string que pode ser armazenada ou transmitida.
+  // No caso de JavaScript, a serialização geralmente se refere à conversão de um objeto JavaScript em uma string JSON.
+  // Este método controla como o objeto é serializado, retornando um novo objeto que contém apenas as propriedades que queremos incluir na string JSON.
+  toJSON() {
+    // Retorna um novo objeto contendo apenas as propriedades que queremos incluir na string JSON
+    return {
+      id: this.id,
+      saldo: this.saldo,
+      numeroConta: this.numeroConta,
+      nomeBanco: this.nomeBanco,
+      boletos: this.boletos.map(boleto => ({
+        id: boleto.id,
+        valor: boleto.valor,
+        dataVencimento: boleto.dataVencimento,
+        pago: boleto.pago
+      }))
+    };
   }
 }
 
